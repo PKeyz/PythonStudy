0dd6534f00f37690e9a00c23e4bf401a15c224e7
Philipp Keyzman <real.phil94@gmail.com>
2024-06-06 22:04:22 +0200

    basic tkInter button

--- 100daysOfPython-Yu/d27_Tkinter_args_kwargs/main.py
+++ 100daysOfPython-Yu/d27_Tkinter_args_kwargs/main.py
@@ -9,5 +9,16 @@ my_label = tk.Label(text="This is a label", font=("Arial", 24, "italic"))
 my_label.pack(side='top')
 
 
+#button
+def button_clicked():
+    my_label.config(text=input.get())
+
+button = tk.Button(text="Click Me", command=button_clicked)
+button.pack(side='top')
+
+# Entry
+input = tk.Entry(width=10)
+input.pack()
+input.get()
 
 window.mainloop()
--- 100daysOfPython-Yu/d27_Tkinter_args_kwargs/playground.py
+++ 100daysOfPython-Yu/d27_Tkinter_args_kwargs/playground.py
@@ -1,9 +1,20 @@
-def add(*args):
-    sum = 0
-    for arg in args:
-        sum += arg
-    return sum
+# def add(*args):
+#     sum = 0
+#     for arg in args:
+#         sum += arg
+#     return sum
+#
+#
+# func_sum = add(1, 5, 6, 10)
+# print(func_sum)
 
 
-func_sum = add(1, 5, 6, 10)
-print(func_sum)
+def calculate(n, **kwargs):
+    n += kwargs.get('add')
+    n *= kwargs.get('multiply')
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
