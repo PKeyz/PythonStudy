ce958ceae6a6b8c84386e6cbd426986cd3d736a1
PhilKeyz <real.phil94@gmail.com>
2025-10-15 15:39:54 +0200

    added imports

--- 100daysOfPython-Yu/7d/hangman.py
+++ 100daysOfPython-Yu/7d/hangman.py
@@ -1,4 +1,6 @@
 import random
+import hangman_img
+
 
 #Reads word_list.txt
 #Adds len(words) >= 4 into word_list [] for later iteration
@@ -13,77 +15,12 @@ with open("word_list.txt","r") as word_text_file:
         if len(line) >= 4:
             word_list.append(line)
 
-#print(word_list) test of word_list[]
-
-#starting img print before loop starts
-hangman_intro_print = r""" /$$   /$$  /$$$$$$  /$$   /$$  /$$$$$$  /$$      /$$  /$$$$$$  /$$   /$$
-| $$  | $$ /$$__  $$| $$$ | $$ /$$__  $$| $$$    /$$$ /$$__  $$| $$$ | $$
-| $$  | $$| $$  \ $$| $$$$| $$| $$  \__/| $$$$  /$$$$| $$  \ $$| $$$$| $$
-| $$$$$$$$| $$$$$$$$| $$ $$ $$| $$ /$$$$| $$ $$/$$ $$| $$$$$$$$| $$ $$ $$
-| $$__  $$| $$__  $$| $$  $$$$| $$|_  $$| $$  $$$| $$| $$__  $$| $$  $$$$
-| $$  | $$| $$  | $$| $$\  $$$| $$  \ $$| $$\  $ | $$| $$  | $$| $$\  $$$
-| $$  | $$| $$  | $$| $$ \  $$|  $$$$$$/| $$ \/  | $$| $$  | $$| $$ \  $$
-|__/  |__/|__/  |__/|__/  \__/ \______/ |__/     |__/|__/  |__/|__/  \__/                                                                    
-"""
-print(hangman_intro_print)
-
-#hangman img list, prints at each step according to user life count
-hangman_pics = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
- /    |
-      |
-=========''','''
-  +---+
-  |   |
-  O   |
- /|\\  |
- / \\  |
-      |
-=========''']
+print(hangman_img.hangman_intro_print)
 
 #Add counter with each mistake
 hangman_count = 0
 
 user_life = 6
-
 
 #Random word choice from word_list[]
 random_word = random.choice(word_list).lower()
@@ -95,7 +32,7 @@ word_to_guess = list("_" * len(random_word_list))
 guessed_letters_list = []
 
 #Init hangman print with count = 0
-print(hangman_pics[hangman_count])
+print(hangman_img.hangman_pics[hangman_count])
 
 
 while (user_life != 0) and (hangman_count < 7) and (random_word_list != word_to_guess):
@@ -108,13 +45,13 @@ while (user_life != 0) and (hangman_count < 7) and (random_word_list != word_to_
 
     if guess_a_letter in guessed_letters_list:
         print(f"You've already guessed {guess_a_letter}")
-        print(hangman_pics[hangman_count])
+        print(hangman_img.hangman_pics[hangman_count])
 
     elif guess_a_letter not in random_word_list:
         print(f"You guessed {guess_a_letter}, that's not in the word. You lose a life.")
         hangman_count += 1
         user_life -= 1
-        print(hangman_pics[hangman_count])
+        print(hangman_img.hangman_pics[hangman_count])
         print(f"****************************{user_life}/6 LIVES LEFT****************************")
 
     elif guess_a_letter in random_word_list:
@@ -127,7 +64,7 @@ while (user_life != 0) and (hangman_count < 7) and (random_word_list != word_to_
     guessed_letters_list.append(guess_a_letter)
 
 if user_life == 0:
-    print(hangman_pics[hangman_count])
+    print(hangman_img.hangman_pics[hangman_count])
     print(f"****************************NO LIVES LEFT, YOU LOSE! ****************************")
 
 if random_word_list == word_to_guess:
