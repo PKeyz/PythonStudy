98d1c967c09858a9fc4d029cdb137ec788781c48
PhilKeyz <real.phil94@gmail.com>
2026-03-06 23:03:58 +0100

    wrote comment-script for higher-lower game

--- 100daysOfPython-Yu/14d/higher_lower.py
+++ 100daysOfPython-Yu/14d/higher_lower.py
@@ -1,8 +1,37 @@
-import pandas_script as ps
+from pandas.core.ops import logical_op
+
+import pandas_script
+
 
 #TODO 1: List of People
 #TODO 2: Generate random Number to assign it to every one of the people
-#TODO 3:
+#TODO 3: Deconstruct the game: Think of relevant functions parameters and variable_names
+
+set magic numbers
+set score variable
+
+define functions here
+
+print ascii
+
+enter loop based on boolean e.g. UserGuessCorrect = True
+
+print Compare A + their attr + their attr. values
+print Compare B + their attr + their attr. values
+compare who has higher value as float
+save as some higher/winner variable
+take user input as A | B .toLowerCase
+if value not A | B .. prompt to repeat input
+elif wrong guess  !+ higher value/winner :
+
+    exit loop
+
+    print You lost with score x text
+else: score += 1
+    A = last winner
+    B = new random entry from list
+    repeat loop
+
 
 # welcome message to 'game'
 print('WELCOME TO THE HIGHER&LOWER GAME')
