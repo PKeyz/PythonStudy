a2fc2ab716a8a9e075f4dda192592c347530d672
PhilKeyz <real.phil94@gmail.com>
2026-03-06 22:47:39 +0100

    upd init welcome message

--- 100daysOfPython-Yu/14d/celebreties_dict.py
+++
@@ -1,5 +0,0 @@
-celebreties = {
-    "username" : [],
-
-
-}
--- 100daysOfPython-Yu/14d/higher_lower.py
+++ 100daysOfPython-Yu/14d/higher_lower.py
@@ -1,8 +1,11 @@
+import pandas_script as ps
+
 #TODO 1: List of People
 #TODO 2: Generate random Number to assign it to every one of the people
 #TODO 3:
 
-# print into ascii
+# welcome message to 'game'
+print('WELCOME TO THE HIGHER&LOWER GAME')
 # guess who has more followers in M  - A or B
 #
 # when False -> Print you lost + score of right answers
@@ -10,4 +13,7 @@
 # if right B becomes new A + new B is generated from dict
 #     repeat the cycle
 #     += 1 to score for each right guess
+
+#     terminate game when guess = False
+#     display score
 
