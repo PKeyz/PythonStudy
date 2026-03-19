019b23af7f03a7b5c484855534f519b7189d9fb3
PhilKeyz <real.phil94@gmail.com>
2026-03-19 20:59:36 +0100

    refund updates

--- 100daysOfPython-Yu/15d/coffe_machine_main.py
+++ 100daysOfPython-Yu/15d/coffe_machine_main.py
@@ -11,16 +11,12 @@ LATTE                   = "Latte"
 CAPPUCCINO              = "Cappuccino"
 REPORT                  = "Report"
 OFF                     = "Off"
+VALUE                   = "value"
 PENNY                   = "Penny"
 NICKEL                  = "Nickel"
 DIME                    = "Dime"
 QUARTER                 = "Quarter"
-#to delete!
-RESOURCE_LIST_WATER     = 0
-RESOURCE_LIST_MILK      = 1
-RESOURCE_LIST_COFFEE    = 2
-RESOURCE_LIST_PRICE     = 3
-
+PROVIDER_TEL            = "+01526374"
 
 menu = {
     "Espresso":{
@@ -43,30 +39,31 @@ menu = {
         }
 }
 
-monetary_value = {
-    "Penny"     :   {"value" : 0.01},
-    "Nickel"    :   {"value" : 0.05},
-    "Dime"      :   {"value" : 0.1},
-    "Quarter"   :   {"value" : 0.25}
-}
-
 machine_resources = {
-    "water" :   50,
-    "coffee":   100,
+    "water" :   500,
     "milk"  :   200,
-    "money" :   0
+    "coffee":   100,
+    "money" :   200
 }
 
+monetary_value = {
+    "Quarter"   :   {"value" : 0.25},
+    "Dime"      :   {"value" : 0.1},
+    "Nickel"    :   {"value" : 0.05},
+    "Penny"     :   {"value" : 0.01},
+}
 
-ON = True
+MACHINE_RUNNING = True
 
 # 2. Turn off the Coffee Machine by entering “off” to the prompt.
 # a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
 # the machine. Your code should end execution when this happens.
-def shut_down_machine():
+def shut_down_machine(has_resources:bool):
     """Shut down the machine"""
-    global ON
-    ON = False
+    resources = has_resources
+    if not resources:
+        global MACHINE_RUNNING
+        MACHINE_RUNNING = False
 
 # 3. Print report.
 # a. When the user enters “report” to the prompt, a report should be generated that shows
@@ -96,7 +93,8 @@ def get_user_choice_resources(user_input):
     elif user_choice == REPORT:
         send_report()
     elif user_choice == OFF:
-        shut_down_machine()
+        keep_running = False
+        shut_down_machine(keep_running)
 
 # 4. Check resources sufficient?
 # a. When the user chooses a drink, the program should check if there are enough
@@ -109,23 +107,27 @@ def insufficient_resources_feedback(user_input):
     """Print statements for def check sufficient_resources"""
     if machine_resources[WATER] <= menu[user_input][WATER]:
         print("Sorry, there is not enough water.\n")
-    elif machine_resources[WATER] <= menu[user_input][MILK]:
+    elif machine_resources[MILK] <= menu[user_input][MILK]:
         print("Sorry, there is not enough milk.\n")
-    elif machine_resources[WATER]<= menu[user_input][COFFEE]:
+    elif machine_resources[COFFEE]<= menu[user_input][COFFEE]:
         print("Sorry, there is not enough coffee.\n")
+
+
 
 def check_sufficient_resources(user_input):
     """Checks if machine has sufficient resources to  produce user selection"""
-    if (machine_resources[WATER]>= menu[user_input][WATER]) and (machine_resources[WATER] >= menu[user_input][MILK]) and (machine_resources[COFFEE] >= menu[user_input][COFFEE]):
+    if (user_input == REPORT) or (user_input == OFF):
+        pass
+    elif  (machine_resources[WATER]>= menu[user_input][WATER]) and (machine_resources[MILK] >= menu[user_input][MILK]) and (machine_resources[COFFEE] >= menu[user_input][COFFEE]):
         return True
     else:
         insufficient_resources_feedback(user_input)
         return False
 
-def deduct_machine_resources(resource_list):
-    machine_resources[WATER] -= resource_list[0]
-    machine_resources[WATER] -= resource_list[1]
-    machine_resources[WATER] -= resource_list[2]
+def deduct_machine_resources(user_input):
+    machine_resources[WATER] -= menu[user_input][WATER]
+    machine_resources[MILK] -= menu[user_input][MILK]
+    machine_resources[COFFEE] -= menu[user_input][COFFEE]
 
 # 5. Process coins.
 # a. If there are sufficient resources to make the drink selected, then the program should
@@ -138,16 +140,43 @@ def print_price(user_input):
 # c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
 # pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
 def calculate_user_payment():
-    user_quarters   = int(input("How many quarters?: "))
-    user_dimes      = int(input("How many dimes?: "))
-    user_nickles    = int(input("How many nickles?: "))
-    user_pennies    = int(input("How many pennies?: "))
-    user_payment = (user_quarters * 0.25) + (user_dimes * 0.1) + (user_nickles * 0.05) + (user_pennies * 0.01)
+    user_quarters   = float(input(f"How many {QUARTER}s?: "))
+    user_dimes      = float(input(f"How many {DIME}s?: "))
+    user_nickles    = float(input(f"How many {NICKEL}s?: "))
+    user_pennies    = float(input(f"How many {PENNY}s?: "))
+    user_payment = (user_quarters * float(monetary_value[QUARTER][VALUE])) + (user_dimes * float(monetary_value[DIME][VALUE])) + (user_nickles * float(monetary_value[NICKEL][VALUE])) + (user_pennies * float(monetary_value[PENNY][VALUE]))
     return user_payment
 
 def process_payment(user_payment,user_input):
-    if user_payment < menu[user_input][MONEY]:
+    if user_payment >= menu[user_input][PRICE]:
         machine_resources[MONEY] += menu[user_input][PRICE]
+        print(f"Thank you! Your {user_input} will the brewed in a second =)")
+        return True
+    else:
+        print(f"Sorry, ${user_payment} is not enough. A {user_input} costs ${menu[user_input][PRICE]}.")
+        return False
+
+def process_refund(user_payment, user_input):
+    surplus = user_payment - menu[user_input][PRICE]
+    refund = surplus
+    total_sum = 0
+    if machine_resources[MONEY] < refund:
+        machine_resources[MONEY] -= refund
+        print(f"Sorry, there is not enough money to refund the full sum.\n please take {machine_resources[MONEY]} and call {PROVIDER_TEL} to get the full sum.")
+    elif machine_resources[MONEY] >= refund:
+        for coin,coin_info in monetary_value.items():
+            coin_value = coin_info[VALUE]
+            if refund >= coin_value:
+                coin_count = int(refund // coin_value)
+                coin_sum = coin_count * coin_value
+                refund -= coin_value * coin_count
+                machine_resources[MONEY] -= coin_sum
+                total_sum += coin_sum
+                #print(f"Your refund is ${coin_sum} in {coin_count} {coin} coins.")
+            else:
+                coin_count = 0
+                #print(f"Your refund is ${coin_sum} in {coin_count} {coin} coins.")
+    print(f"Your refund is ${total_sum}.")
 
 # 6. Check transaction successful?
 # a. Check that the user has inserted enough money to purchase the drink they selected.
@@ -181,11 +210,18 @@ def process_payment(user_payment,user_input):
 # latte was their choice of drink
 
 
-user_input = ask_user_choice
+user_input = ask_user_choice()
 get_user_choice_resources(user_input)
+has_sufficient_resources = check_sufficient_resources(user_input)
+shut_down_machine(has_sufficient_resources)
+user_payment = calculate_user_payment()
+is_payment_accepted = process_payment(user_payment,user_input)
+
+process_refund(user_payment,user_input)
 
 
-# while ON:
+
+# while MACHINE_RUNNING:
 #     user_choice_resources = get_user_choice_resources()
 #     print(user_choice_resources)
 #     enough_resources_bool = check_sufficient_resources(user_choice_resources)
